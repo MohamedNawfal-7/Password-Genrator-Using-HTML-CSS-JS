ec074e2d1d8f274bfe30a9b93a4f51924c2b5dfd
Mohamed Nawfal <mohamednawfal183@gmail.com>
2024-08-04 17:50:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('generate').addEventListener('click', function() {
+    const passwordLength = 12;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let password = '';
+    
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+    
+    document.getElementById('password').value = password;
+});
